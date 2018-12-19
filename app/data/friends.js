1e8b46fd44837a3282f167186fa9896fb424416c
Iris Cheng <chengiris66@gmail.com>
2018-12-18 21:08:55 -0800

    Initial version

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsArray = [
+    {
+        name: "Tulip",
+        photo: "https://www.longfield-gardens.com/_ccLib/image/plants/DETA-1037.jpg",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+];
+
+module.exports = friendsArray;

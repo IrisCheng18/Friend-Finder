f6ba0dcfa333282986e05de3cbbd77b432430b9e
Iris Cheng <chengiris66@gmail.com>
2018-12-18 23:17:53 -0800

    Finished

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,6 +14,150 @@ var friendsArray = [
             4,
             1
         ]
+    },
+    {
+        name: "Pink Carnation",
+        photo: "https://cdn.globalrose.com/assets/img/store/best-pink-carnations-globalrose.jpg",
+        scores: [
+            1,
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4
+        ]
+    },
+    {
+        name: "Water Lily",
+        photo: "https://images.homedepot-static.com/productImages/3605b071-7f66-4d17-8010-848d338cd1cb/svn/van-zyverden-water-pond-plants-83620-64_400_compressed.jpg",
+        scores: [
+            4,
+            1,
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5
+        ]
+    },
+    {
+        name: "Morning Glory",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/9/9b/Morning-glory-C6295b.jpg",
+        scores: [
+            5,
+            4,
+            1,
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2
+        ]
+    },
+    {
+        name: "Sunflower",
+        photo: "https://statesymbolsusa.org/sites/statesymbolsusa.org/files/symbol-place-images/PerfectSunflower.jpg",
+        scores: [
+            2,
+            5,
+            4,
+            1,
+            5,
+            1,
+            4,
+            4,
+            5,
+            1
+        ]
+    },
+    {
+        name: "Peony",
+        photo: "https://www.thespruce.com/thmb/vbhm1Jb8OWFxj73Sh5GkawLzwfY=/450x0/filters:no_upscale():max_bytes(150000):strip_icc()/close-up-of-pink-peony-510820237-58b58f243df78cdcd8514fac.jpg",
+        scores: [
+            1,
+            2,
+            5,
+            4,
+            1,
+            5,
+            1,
+            4,
+            4,
+            5
+        ]
+    },
+    {
+        name: "Chrysanthemum",
+        photo: "https://www.gardendesign.com/pictures/images/675x529Max/site_3/cambodian-queen-daisy-mum-alamy-stock-photo_12095.jpg",
+        scores: [
+            5,
+            1,
+            2,
+            5,
+            4,
+            1,
+            5,
+            1,
+            4,
+            4
+        ]
+    },
+    {
+        name: "Lily",
+        photo: "http://hdwpro.com/wp-content/uploads/2018/02/Nice-Lily.jpg",
+        scores: [
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1,
+            5,
+            1,
+            4
+        ]
+    },
+    {
+        name: "Daffodil",
+        photo: "https://images.homedepot-static.com/productImages/4e5bb2e3-fc4f-494c-8652-c2850918199e/svn/bloomsz-flower-bulbs-07589-64_1000.jpg",
+        scores: [
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1,
+            5,
+            1
+        ]
+    },
+    {
+        name: "Daisy",
+        photo: "https://cdn2.stylecraze.com/wp-content/uploads/2013/11/Top-25-Most-Beautiful-Daisy-Flowers.jpg",
+        scores: [
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1,
+            5
+        ]
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,29 @@
 var friendsArray = require("../data/friends");
 
-module.exports = function(app) {
-    app.get("/api/friends", function(req, res) {
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
         res.json(friendsArray);
     });
 
-    app.post("/api/friends", function(req, res) {
-        console.log(req.body);
+    app.post("/api/friends", function (req, res) {
+        var newFriend = req.body;
+
+        var sumArray = [];
+             
+        for (var j = 0; j < friendsArray.length; j++) {
+            var sum = 0;
+
+            for (var i = 0; i < newFriend.scores.length; i++) {
+                sum += Math.abs(parseInt(newFriend.scores[i]) - parseInt(friendsArray[j].scores[i]));
+            }
+
+            sumArray.push(sum);
+        };
+        
+        var index = sumArray.indexOf(Math.min(...sumArray));
+        res.json({
+            name: friendsArray[index].name,
+            photo: friendsArray[index].photo
+        });
     });
 };
